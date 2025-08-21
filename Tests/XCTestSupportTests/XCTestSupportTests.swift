81209c0667ab8576d71f87d5bbb09a92e338f8ab
Jamie Froggatt <james.froggatt@atomicmedia.co.uk>
2025-08-21 13:36:46 +0100

    Add basic compilation test

--- Tests/XCTestSupportTests/XCTestSupportTests.swift
+++ Tests/XCTestSupportTests/XCTestSupportTests.swift
@@ -1,6 +1,13 @@
 import Testing
-@testable import TestSupport
+@testable import XCTestSupport
 
+struct TestRobot: Robot {
+    func doStuff() {}
+}
+
+@MainActor
 @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    // Basic compilation example
+    TestRobot()
+        .doStuff()
 }
