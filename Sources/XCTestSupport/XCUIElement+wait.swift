cb40160c0be7194025e51fd010ad13f804aae7fc
Jamie Froggatt <james.froggatt@atomicmedia.co.uk>
2025-08-21 13:36:32 +0100

    Add wait override taking a key path

--- Sources/XCTestSupport/XCUIElement+wait.swift
+++ Sources/XCTestSupport/XCUIElement+wait.swift
@@ -2,6 +2,11 @@ import XCTest
 
 public extension XCUIElement {
     /// A wait function which accepts waiting for any derived property to be true, and includes a default timeout of 1s.
+    func wait(for property: KeyPath<XCUIElement, Bool>, timeout: TimeInterval = 1) -> Bool {
+        wait(for: { $0[keyPath: property] }, timeout: timeout)
+    }
+    
+    /// A wait function which accepts waiting for any derived property to be true, and includes a default timeout of 1s.
     func wait(for property: @escaping (Self) -> Bool, timeout: TimeInterval = 1) -> Bool {
         let result = XCTWaiter().wait(
             for: [
