d561303cc975464044465f03eee50171e02e07f3
Christopher Wainwright <chris.wainwright@atomicmedia.co.uk>
2025-10-20 09:46:11 +0100

    NetMock 3.0.0 Update (#4)
    NetMock 3.0.0 Update
    * Update to NetMock dependency to 3.0.0
    * Update ReadMe

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             targets: ["XCTestSupport"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/TeamAtomicMedia/NetMock-iOS.git", .upToNextMajor(from: "2.0.0"))
+        .package(url: "https://github.com/TeamAtomicMedia/NetMock-iOS.git", from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
